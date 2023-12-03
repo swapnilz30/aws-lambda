55a8d853b963ad62c1f405e2d081537564b59d89
Swapnil <swapnilz30@users.noreply.github.com>
2023-12-03 22:19:27 +0530

    added lambda_aws_python_http_api

--- lambda_aws_python_http_api/handler.py
+++ lambda_aws_python_http_api/handler.py
@@ -9,7 +9,7 @@ import dynamo # helper function
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-dynamo = boto3.client('dynamodb')
+dynamodb = boto3.client('dynamodb')
 table_name = str(os.environ['DYNAMODB_TABLE'])
 
 def create(event, context):
@@ -23,10 +23,113 @@ def create(event, context):
     post_str = event['body']
     post = json.loads(post_str)
     current_timestamp = datetime.now().isoformat()
-    post['createAt'] = current_timestamp
-    post['id'] = str(uuid.uuid4)
+    post['createdAt'] = current_timestamp
+    post['id'] = str(uuid.uuid4())
 
     res = dynamodb.put_item(
         TableName=table_name,
         Item=dynamo.to_item(post)
     )
+    if res['ResponseMetadata']['HTTPStatusCode'] == 200:
+        response = {
+            "statusCode": 201,
+        }
+    return response    
+
+
+def get(event, context):
+    print(":::::==>>>", event['pathParameters'])
+    logger.info(f'Incoming request is: {event}')
+    response = {
+        "statusCode": 500,
+        "body": "An error occured while getting post."
+    }
+    print(":::::==>>>", event)
+
+    post_id = event['pathParameters']['postId']
+
+    post_query = dynamodb.get_item(
+        TableName=table_name, Key={'id': {'S': post_id}}
+    )
+
+    if 'Item' in post_query:
+        post = post_query['Item']
+        logger.info(f'Post is: {post}')
+        response = {
+            "statusCode": 200,
+            'headers': {'Content-Type': 'application/json'},
+            "body": json.dumps(dynamo.to_dict(post))
+        }
+    return response
+
+def all(event, context):
+    response = {
+        "statusCode": 500,
+        "body": "An error occured while getting all posts."
+    }
+    scan_result = dynamodb.scan(TableName=table_name)['Items']
+    print("scan_result::::", scan_result)
+
+    posts = []
+
+    for item in scan_result:
+        posts.append(dynamo.to_dict(item))
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(posts)
+    }
+
+    return response
+
+def update(event, context):
+    logger.info(f'Incoming request is: {event}')
+    post_id = event['pathParameters']['postId']
+
+    response = {
+        "statusCode": 500,
+        "body": f"An error occured while updating post {post_id}"
+    }
+
+    post_str = event['body']
+    post = json.loads(post_str)
+
+    res = dynamodb.update_item(
+        TableName=table_name,
+        Key={
+            'id': {'S': post_id}
+        },
+        UpdateExpression="set content=:c, author=:a, updatedAt=:u",
+        ExpressionAttributeValues={
+            ':c': dynamo.to_item(post['content']),
+            ':a': dynamo.to_item(post['author']),
+            ':u': dynamo.to_item(datetime.now().isoformat())
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+
+    if res['ResponseMetadata']['HTTPStatusCode'] == 200:
+        response = {
+            "statusCode": 200,
+        }
+
+    return response
+
+def delete(event, context):
+    logger.info(f'Incoming request is: {event}')
+
+    post_id = event['pathParameters']['postId']
+
+    response = {
+        "statusCode": 500,
+        "body": f"An error occured while deleting post {post_id}"
+    }
+
+    res = dynamodb.delete_item(TableName=table_name, Key={
+        'id': {'S': post_id}})
+
+    if res['ResponseMetadata']['HTTPStatusCode'] == 200:
+        response = {
+            "statusCode": 204,
+        }
+    return response    
